b2515984a3663728817bef0c81789959cadffe09
Nokwanda Khubisa <Nokwanda.Khubisa@standardbank.co.za>
2024-01-11 08:42:49 +0200

    Add project files.

---
+++ 12.If Statements2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.If_Statements2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
